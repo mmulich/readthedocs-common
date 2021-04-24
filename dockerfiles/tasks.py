954890be64563b3cc33337e9f1be2270a75f0a6b
Michael Mulich <michael.mulich@gmail.com>
2021-04-24 03:46:33 -0700

    enable docker.up to detach from docker-compose up

--- dockerfiles/tasks.py
+++ dockerfiles/tasks.py
@@ -41,7 +41,7 @@ def down(c, volumes=False):
     'ext-theme': 'Enable new theme from ext-theme (default: False)',
     'scale-build': 'Add additional build instances (default: 1)',
 })
-def up(c, search=True, init=False, reload=True, webpack=False, ext_theme=False, scale_build=1):
+def up(c, search=True, init=False, reload=True, webpack=False, ext_theme=False, scale_build=1, detach=False):
     """Start all the docker containers for a Read the Docs instance"""
     cmd = []
 
@@ -61,6 +61,8 @@ def up(c, search=True, init=False, reload=True, webpack=False, ext_theme=False, 
         cmd.insert(0, 'RTD_EXT_THEME_DEV_SERVER_ENABLED=t')
     if ext_theme:
         cmd.insert(0, 'RTD_EXT_THEME_ENABLED=t')
+    if detach:
+        cmd.append('--detach')
 
     cmd.append('up')
 
